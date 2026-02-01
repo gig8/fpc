e1aa3afb081ab0a7e312a9a5439250cdb313df1a
Tim Uy <tim@gig8.com>
2026-01-31 20:39:07 -0800

    ARM64: fresh UnwindHistory for RtlUnwindEx; add DumpFrames debug to arm64trap
    - Reset UnwindHistory before passing to RtlUnwindEx so our RtlVirtualUnwind
      calls don't pollute the OS unwinder's state
    - Add DumpFrames at 6 points (try entry, in try, in finally, after finally,
      back in main, after Flush) to trace stack at each stage

--- tests/phase2-tests/arm64trap.pas
+++ tests/phase2-tests/arm64trap.pas
@@ -7,22 +7,40 @@
 program arm64trap;
 {$mode objfpc}
 
+procedure DumpFrames(const tag: string);
+var
+  frames: array[0..31] of CodePointer;
+  n, i: longint;
+begin
+  n := CaptureBacktrace(1, 32, @frames[0]);
+  writeln(stderr, 'DEBUG FRAMES [', tag, '] count=', n);
+  for i := 0 to n - 1 do
+    writeln(stderr, '  ', i, ': ', BackTraceStrFunc(frames[i]));
+  Flush(stderr);
+end;
+
 procedure TestException;
 begin
+  DumpFrames('1. TestException entry (before try)');
   try
     writeln('Entering try block...');
+    DumpFrames('2. In try block, about to exit');
     exit;
   finally
     writeln('Success: Finally block executed!');
+    DumpFrames('3. In finally block');
   end;
   writeln('DEBUG: After finally block in TestException');  { if missing, crash at RtlUnwindEx transfer or first instr after try }
+  DumpFrames('4. After finally, before return');
 end;
 
 begin
   TestException;
   writeln('DEBUG: Back in main (after TestException)');  { if missing, crash on return from TestException }
+  DumpFrames('5. Back in main');
   Flush(Output);
   writeln('DEBUG: After Flush(Output)');                  { if missing, crash in Flush }
+  DumpFrames('6. After Flush');
   writeln('Done.');
   Flush(Output);
 end.
